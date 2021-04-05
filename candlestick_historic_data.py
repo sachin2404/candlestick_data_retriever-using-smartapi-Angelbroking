f5f6130cb18e5ed5bcfa1c66da0d88c571428ea6
Deep Patel <sachin2404@users.noreply.github.com>
2021-04-05 14:43:59 +0530

    Add files via upload

---
+++ candlestick_historic_data.py
@@ -0,0 +1,41 @@
+#package import statement
+from smartapi import SmartConnect
+from smartapi import WebSocket
+import pandas as pd
+import csv
+#create object of call
+try:
+    obj=SmartConnect(api_key="SfYPpk6k")
+    #login api call
+    data = obj.generateSession("PTAJ1165","hpcr19071624")
+    refreshToken= data['data']['refreshToken']
+    #fetch the feedtoken
+    feedToken=obj.getfeedToken()
+    #fetch User Profile
+    userProfile= obj.getProfile(refreshToken)
+except Exception as e:
+    print("Login Failed: {}".format(e.message))
+#symboltoken : check your instrument on this site. "https://margincalculator.angelbroking.com/OpenAPI_File/files/OpenAPIScripMaster.json"
+try:
+    historicParam={
+    "exchange": "NSE",
+    "symboltoken": "3045",
+    "interval": "ONE_MINUTE",
+    "fromdate": "2021-03-01 09:15", 
+    "todate": "2021-03-31 15:30"
+    }
+    data_csv = obj.getCandleData(historicParam)
+    arr = data_csv['data'].split("\n")
+    with open("data.csv","w",newline='') as file:
+        writer = csv.writer(file)
+        for i in arr:
+            i = i.split(",")
+            writer.writerow(i)
+except Exception as e:
+    print("Historic Api failed: {}".format(e.message))
+    
+try:
+    logout=obj.terminateSession('PTAJ1165')
+    print("Logout Successfull")
+except Exception as e:
+    print("Logout failed: {}".format(e.message))    
